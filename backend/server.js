d36e022b4234d9872dc469a460848ddd80d9aae8
rizwanishaq <rizwanishaq@gmail.com>
2022-01-27 11:53:04 +0100

    backend completed

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const colors = require("colors");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
+
+// Connect to database
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Welcome to the Support Desk API" });
+});
+
+// Routes
+app.use("/api/users", require("./routes/userRoutes"));
+
+app.use(errorHandler);
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Server started on port ${process.env.PORT}`);
+});
